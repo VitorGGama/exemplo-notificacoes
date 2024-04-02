a7fb64e7fa45f5c1e0958b35acfd7f66f18cfd05
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-04-02 11:30:21 -0300

    UpDate

--- App.js
+++ App.js
@@ -1,15 +1,81 @@
-import { Button, StatusBar, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar, Button } from "react-native";
+import { useState, useEffect } from "react";
+
+// expo-notifications
 import * as Notifications from "expo-notifications";
 
+// Manipulando o evento de notificação
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: false,
+    };
+  },
+});
+
 export default function App() {
-  const enviarMensagem = () => {};
+  // State da notificação
+  const [dados, setDados] = useState(null);
+
+  // Effect de permissões
+  useEffect(() => {
+    // Função de permissão IOS
+    async function permissoesIOS() {
+      return await Notifications.requestPermissionsAsync({
+        ios: {
+          allowAlert: true,
+          allowSound: true,
+          allowBadge: true,
+        },
+      });
+    }
+    permissoesIOS();
+
+    // Alert no topo do aplicativo
+    Notifications.addNotificationReceivedListener((notificacao) => {
+      console.log(notificacao);
+    });
+
+    // Interação do usuario com a notificação
+    Notifications.addNotificationResponseReceivedListener((resposta) => {
+      setDados(resposta.notification.request.content.data); // resposta recebendo objeto data
+    });
+  }, []);
+
+  // Função para enviar notificação local
+  const enviarNotificacao = async () => {
+    // Mensagem enviada
+    const mensagem = {
+      title: "Lembrete!",
+      body: "Hoje tem Corinthians",
+      data: {
+        usuario: "Vitor",
+        cidade: "Coringão",
+      },
+    };
+
+    // Função de agendamento de notificações
+    await Notifications.scheduleNotificationAsync({
+      content: mensagem,
+      trigger: { seconds: 1 },
+    });
+  };
 
   return (
     <>
       <StatusBar />
       <View style={styles.container}>
-        <Text>Exemplo de notificação local</Text>
-        <Button title="Disparar notificação" onPress={enviarMensagem} />
+        <Text>Recurso de Notificação Local</Text>
+        <Button title="Disparar notificação" onPress={enviarNotificacao} />
+
+        {dados && (
+          <View style={styles.viewDados}>
+            <Text style={styles.textoDados}>Usuario: {dados.usuario}</Text>
+            <Text style={styles.textoDados}>Cidade: {dados.cidade}</Text>
+          </View>
+        )}
       </View>
     </>
   );
@@ -21,5 +87,17 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    margin: 10,
+  },
+
+  viewDados: {
+    marginVertical: 10,
+    backgroundColor: "darkblue",
+    padding: 10,
+  },
+
+  textoDados: {
+    fontSize: 14,
+    color: "white",
   },
 });
